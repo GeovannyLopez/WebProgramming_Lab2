30033a6f30ce3772790d275ce09afef5f0632f13
Oscar López <oscar.lopez@vivoconsulting.com>
2018-09-16 17:31:53 -0600

    Modificación de vista para utilizar React

--- lab2/src/index.js
+++ lab2/src/index.js
@@ -4,5 +4,50 @@ import './index.css';
 import MusicLibrary from './MusicLibrary';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<MusicLibrary />, document.getElementById('root'));
+
+const state ={
+    menuData: 
+    {
+        userName : 'Geovanny',
+        songs: [
+            {
+                id : 1,
+                name : '10 AM',
+                artist : 'PXNDX',
+                album : 'Sangre Fría',
+                year : 2013,
+            },
+            {
+                id : 2,
+                name : 'Enfermedad en casa',
+                artist : 'PXNDX',
+                album : 'Sangre Fría',
+                year : 2013,
+            },
+            {
+                id : 3,
+                name : '3+1',
+                artist : 'PXNDX',
+                album : 'Para ti con desprecio',
+                year : 2005,
+            },
+            {
+                id : 4,
+                name : 'Aforismos',
+                artist : 'PXNDX',
+                album : 'Bonanza',
+                year : 2012,
+            },
+            {
+                id : 5,
+                name : 'La noche de la mesa triste',
+                artist : 'PXNDX',
+                album : 'Bonanza',
+                year : 2012,
+            }
+        ]
+    }
+};
+
+ReactDOM.render(<MusicLibrary {...state}/>, document.getElementById('root'));
 registerServiceWorker();
